2e5f658ac2ef9667da97984cbdc2c49d665c6900
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-14 00:27:03 +0000

    Adding tarfile member sanitization to extractall()

--- annbench/dataset/sift1m.py
+++ annbench/dataset/sift1m.py
@@ -15,7 +15,29 @@ class Sift1m(BaseDataset):
         if not tar_path.exists():
             urlretrieve("ftp://ftp.irisa.fr/local/texmex/corpus/sift.tar.gz", tar_path)
         with tarfile.open(tar_path, 'r:gz') as f:
-            f.extractall(path=self.path)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, path=self.path)
 
     def vecs_train(self):
         vec_path = self.path / "sift/sift_learn.fvecs"
